5fa8015f1a2677cf04f8db3ff5c2fe48c2280f19
Dr-Fortitudo <136475872+Dr-Fortitudo@users.noreply.github.com>
2025-04-07 08:21:23 +0530

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def main():
+    st.title("🏠 Welcome to the Home Page")
+    st.write("This is the homepage of your multi-page Streamlit website.")
+
+if __name__ == "__main__":
+    main()
