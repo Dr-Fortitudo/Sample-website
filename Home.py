98c6fb4343849d80fd33218f5a90cfe937fc7c0f
Dr-Fortitudo <136475872+Dr-Fortitudo@users.noreply.github.com>
2025-04-07 09:19:54 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,9 +1,6 @@
 import streamlit as st
 
-def main():
+def app():
     st.title("🏠 Welcome to the Home Page")
     st.write("Welcome to the homepage of your Streamlit website.")
     st.image("https://plus.unsplash.com/premium_photo-1668017178993-4c8fc9f59872?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", use_container_width=True)
-
-if __name__ == "__main__":
-    main()
