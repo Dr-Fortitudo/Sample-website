45803435797d7562157258bd4e5c0fb65d7118a8
Dr-Fortitudo <136475872+Dr-Fortitudo@users.noreply.github.com>
2025-04-07 08:21:58 +0530

    Create About.py

---
+++ About.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def main():
+    st.title("ℹ️ About Us")
+    st.write("We are building a cool multipage website using Streamlit!")
+
+if __name__ == "__main__":
+    main()
