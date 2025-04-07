c799b17e28c6c1518be3209fb66eca192944000f
Dr-Fortitudo <136475872+Dr-Fortitudo@users.noreply.github.com>
2025-04-07 09:15:06 +0530

    Update About.py

--- About.py
+++ About.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 def main():
     st.title("ℹ️ About Us")
-    st.write("We are building a cool multipage website using Streamlit!")
-
+    st.write("We are building a multi-page website in Streamlit using a custom navbar.")
+    st.success("This approach allows a more web-like experience in Streamlit!")
+    
 if __name__ == "__main__":
     main()
